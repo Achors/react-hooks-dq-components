9a0dba7f5b2648df47325d0bb88679129659218c
Adolf <adolfnyerere@gmail.com>
2023-10-24 04:20:27 +0300

    added components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import TopMenu from "./TopMenu";
+import MenuLinks from "./MenuLinks";
+import MainContent from "./MainContent";
+import SideMenu from "./SideMenu";
+import Logo from "./Logo";
+import GenrePicker from "./GenrePicker";
+import AlbumCard from "./AlbumCard";
+import AlbumList from "./AlbumList";
+import Search from "./Search";
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return <div className="app-container">
+    <TopMenu />
+    <SideMenu />
+    <MenuLinks />
+    <MainContent />
+    <Search />
+    <AlbumCard />
+    <AlbumList />
+    <GenrePicker />
+    <Logo />
+  </div>;
 }
 
 export default App;
